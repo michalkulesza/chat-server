6948f4ef07c3cd17eb778382e51573e9c6c106b8
unknown <kulesza.michal@gmail.com>
2020-05-25 19:18:53 +0200

    Styling and error on input

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on("connect", socket => {
+	console.log("User connected");
+
+	socket.on("disconnect", () => {
+		console.log("User disconnected");
+	});
+});
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));

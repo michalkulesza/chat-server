5dc631eb298ffdfdf99ec4b997b2053776e42e8f
unknown <kulesza.michal@gmail.com>
2020-05-26 11:55:52 +0200

    Join and disconnect added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const socketio = require("socket.io");
 const http = require("http");
 const router = require("./router");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
@@ -11,10 +13,51 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connect", socket => {
-	console.log("User connected");
+	socket.on("join", ({ name, room }, callback) => {
+		const { error, user } = addUser({ id: socket.id, name, room });
+
+		if (error) return callback(error);
+
+		socket.emit("message", {
+			name: "admin",
+			text: `${user.name} welcome to the "${user.room}" room `,
+		});
+		socket.broadcast
+			.to(user.room)
+			.emit("message", { name: "admin", text: `${user.name} has joined` });
+
+		socket.join(user.room);
+
+		io.to(user.room).emit("roomData", {
+			room: user.room,
+			users: getUsersInRoom(user.room),
+		});
+
+		callback();
+	});
+
+	socket.on("sendMessage", (message, name, callback) => {
+		const user = getUser(socket.id);
+
+		io.to(user.room).emit("message", { text: message, name });
+
+		io.to(user.room).emit("roomData", {
+			room: user.room,
+			users: getUsersInRoom(user.room),
+		});
+
+		callback();
+	});
 
 	socket.on("disconnect", () => {
-		console.log("User disconnected");
+		const user = removeUser(socket.id);
+
+		if (user) {
+			io.to(user.room).emit("message", {
+				user: "admin",
+				text: `${user.name} has left.`,
+			});
+		}
 	});
 });
 

483f18f66b43e8c26318438c1678a8d227359679
unknown <kulesza.michal@gmail.com>
2020-05-29 17:07:17 +0200

    misc

--- db/db.js
+++ db/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-console.log(process.env.MONGODB_URL);
-
 mongoose
 	.connect(process.env.MONGODB_URL, {
 		useNewUrlParser: true,

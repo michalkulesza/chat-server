54a6a46b57c83d13012f8d9d35c0aaa27ba982c8
Michal <kulesza.michal@gmail.com>
2020-11-29 16:45:06 +0100

    add: spcket decryption

--- app.js
+++ app.js
@@ -1,108 +1,98 @@
+require("dotenv").config();
 const express = require("express");
 const socketio = require("socket.io");
+const encrypt = require("socket.io-encrypt");
 const http = require("http");
 const router = require("./router");
-const { MessageModel } = require("./models/Models");
-require("./db/db");
-require("dotenv").config();
 
-const {
-	addUser,
-	removeUser,
-	getUser,
-	getUsersIdByName,
-	getUsersInRoom,
-} = require("./users");
+// const { MessageModel } = require("./models/Models");
+
+require("./db/db");
+
+// const { addUser, removeUser, getUser, getUsersIdByName, getUsersInRoom } = require("./users");
 
 const PORT = process.env.PORT || 5001;
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+io.use(encrypt("secret"));
 
 io.on("connect", socket => {
-	socket.on("initJoin", ({ name, initRoom }) => {
-		const { error, user } = addUser({ id: socket.id, name, room: initRoom });
-		if (error) return callback(error);
+	console.log("Socket Connected");
 
-		socket.join(initRoom);
-		io.to(initRoom).emit("roomData", {
-			room: initRoom,
-			users: getUsersInRoom(initRoom),
-		});
+	socket.on("join", ({ username }) => {
+		console.log(`${username} has joined.`);
 
-		socket.emit("initData", "Main");
+		socket.emit("authSuccessfull");
 	});
 
-	socket.on("readyForInitData", initRoom => {
-		socket.emit("getData", initRoom);
-	});
+	// socket.on("readyForInitData", initRoom => {
+	// 	socket.emit("getData", initRoom);
+	// });
 
-	socket.on("joinChat", ({ roomName }) => {
-		socket.join(roomName);
-		socket.emit("getData", roomName);
-	});
+	// socket.on("joinChat", ({ roomName }) => {
+	// 	socket.join(roomName);
+	// 	socket.emit("getData", roomName);
+	// });
 
-	socket.on("leaveChat", () => {
-		socket.leaveAll();
-	});
+	// socket.on("leaveChat", () => {
+	// 	socket.leaveAll();
+	// });
 
-	socket.on("ready", (name, roomName) => {
-		socket.broadcast.to(roomName).emit("message", {
-			text: `${name} has joined.`,
-			name: "admin",
-		});
-		socket.emit("message", {
-			text: "You have joined.",
-			name: "admin",
-		});
-	});
+	// socket.on("ready", (name, roomName) => {
+	// 	socket.broadcast.to(roomName).emit("message", {
+	// 		text: `${name} has joined.`,
+	// 		name: "admin",
+	// 	});
+	// 	socket.emit("message", {
+	// 		text: "You have joined.",
+	// 		name: "admin",
+	// 	});
+	// });
 
-	socket.on("sendMessage", async (message, name, room) => {
-		const date = new Date();
-		date.setSeconds(0, 0);
-		const timeStamp = date.toISOString();
+	// socket.on("sendMessage", async (message, name, room) => {
+	// 	const date = new Date();
+	// 	date.setSeconds(0, 0);
+	// 	const timeStamp = date.toISOString();
 
-		io.sockets.in(room).emit("message", {
-			text: message,
-			name,
-			timestamp: timeStamp,
-			room,
-		});
+	// 	io.sockets.in(room).emit("message", {
+	// 		text: message,
+	// 		name,
+	// 		timestamp: timeStamp,
+	// 		room,
+	// 	});
 
-		const aMessage = new MessageModel({
-			name: name,
-			text: message,
-			timestamp: timeStamp,
-		});
+	// 	const aMessage = new MessageModel({
+	// 		name: name,
+	// 		text: message,
+	// 		timestamp: timeStamp,
+	// 	});
 
-		aMessage.addMessage(room, aMessage);
-	});
+	// 	aMessage.addMessage(room, aMessage);
+	// });
 
-	socket.on("disconnect", () => {
-		const user = removeUser(socket.id);
+	// socket.on("disconnect", () => {
+	// 	const user = removeUser(socket.id);
 
-		if (user) {
-			io.to(user.room).emit("roomData", {
-				room: user.room,
-				users: getUsersInRoom(user.room),
-			});
+	// 	if (user) {
+	// 		io.to(user.room).emit("roomData", {
+	// 			room: user.room,
+	// 			users: getUsersInRoom(user.room),
+	// 		});
 
-			socket.broadcast.to(user.room).emit("message", {
-				text: `${user.name} has left.`,
-				name: "admin",
-			});
-		}
-	});
+	// 		socket.broadcast.to(user.room).emit("message", {
+	// 			text: `${user.name} has left.`,
+	// 			name: "admin",
+	// 		});
+	// 	}
+	// });
 });
 
 app.use(express.json());
 app.use(function (req, res, next) {
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"X-Requested-With,content-type,Authorization"
-	);
+	res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,Authorization");
 	next();
 });
 
